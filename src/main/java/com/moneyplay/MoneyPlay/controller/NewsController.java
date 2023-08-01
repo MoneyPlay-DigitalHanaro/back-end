cd0c6081f9fafeaea774bd2dc128db8904a255f6
YEJIN325 <seoyj0325@naver.com>
2023-08-01 18:00:10 +0900

    뉴스 크롤링 + 뉴스 목록 API + 뉴스 상세 API

---
+++ src/main/java/com/moneyplay/MoneyPlay/controller/NewsController.java
@@ -0,0 +1,44 @@
+package com.moneyplay.MoneyPlay.controller;
+
+import com.moneyplay.MoneyPlay.domain.News;
+import com.moneyplay.MoneyPlay.service.NewsService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/news")
+public class NewsController {
+    private final NewsService newsService;
+
+    public NewsController(NewsService newsService){
+        this.newsService = newsService;
+    }
+
+
+    @GetMapping
+    public ResponseEntity<?> getNewsList() throws Exception {
+        try {
+            List<News> newsList = newsService.getNewsData();
+            return new ResponseEntity<>(newsList, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
+
+    @GetMapping("/detail")
+    // 인덱스를 파라미터로 받아서 뉴스 상세 페이지에 접근
+    public ResponseEntity<?> getNewsDetailPage(@RequestParam Long index) throws Exception {
+        try {
+            News newsDetail = newsService.getNewsDetail(index);
+            return new ResponseEntity<>(newsDetail, HttpStatus.OK);
+        } catch (Exception e) {
+            return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
+        }
+    }
+}
