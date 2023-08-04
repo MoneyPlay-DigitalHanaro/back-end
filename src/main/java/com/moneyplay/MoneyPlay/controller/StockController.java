47d0fd783c7e7833c3ab103390e8fdcf3ba5a8e6
IkHwan Kim <52392858+dlrghks2090@users.noreply.github.com>
2023-08-04 16:22:04 +0900

    #13 #14 한국투자증권 open API 접근 토큰 발급 (#15)
    * feat: #13 #14  한국 투자증권 api 토큰 받아오기 성공

--- src/main/java/com/moneyplay/MoneyPlay/controller/StockController.java
+++ src/main/java/com/moneyplay/MoneyPlay/controller/StockController.java
@@ -1,5 +1,6 @@
 package com.moneyplay.MoneyPlay.controller;
 
+import com.moneyplay.MoneyPlay.domain.dto.StockAPITokenDto;
 import com.moneyplay.MoneyPlay.service.StockService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -17,10 +18,12 @@ public class StockController {
     private final StockService stockService;
 
     // 국내 주식 시세 조회 URL
-    @GetMapping("/{id}")
-    public ResponseEntity<?> threeMonthStockDayChart(@PathVariable Long id) {
+    @GetMapping
+    public ResponseEntity<?> threeMonthStockDayChart() {
         try {
-            return new ResponseEntity<>(, HttpStatus.OK); //추가해야함 데이터
+            StockAPITokenDto stockAPITokenDto = stockService.getApiToken();
+            System.out.println("API token 받아옴!");
+            return new ResponseEntity<>(stockAPITokenDto, HttpStatus.OK); //추가해야함 데이터
         } catch (Exception e) {
             return new ResponseEntity<>(e.getMessage(), HttpStatus.BAD_REQUEST);
         }
