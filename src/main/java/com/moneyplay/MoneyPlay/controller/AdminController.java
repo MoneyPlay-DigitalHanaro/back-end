f0f36ba639f0482028e2e5f32bcb45f56b9f86ee
Bini <kuninyb@naver.com>
2023-08-11 13:57:18 +0900

    Add: 관리자 포인트 기능 완료

--- src/main/java/com/moneyplay/MoneyPlay/controller/AdminController.java
+++ src/main/java/com/moneyplay/MoneyPlay/controller/AdminController.java
@@ -2,6 +2,7 @@ package com.moneyplay.MoneyPlay.controller;
 
 import com.moneyplay.MoneyPlay.domain.ClassRoom;
 import com.moneyplay.MoneyPlay.domain.User;
+import com.moneyplay.MoneyPlay.domain.dto.AdminDto;
 import com.moneyplay.MoneyPlay.repository.ClassRoomRepository;
 import com.moneyplay.MoneyPlay.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
@@ -9,6 +10,7 @@ import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.web.bind.annotation.*;
 
+import javax.transaction.Transactional;
 import java.util.List;
 
 @CrossOrigin(origins="*")
@@ -35,13 +37,19 @@ public class AdminController {
 
 
     // 포인트 일괄 지급
-
+    @Transactional
     @PostMapping("/admin/total_increase")
-    @Modifying
-    @Query(value = "UPDATE User u SET u.point.holdingPoint = :increase_point + u.point.holdingPoint")
-    public List<User> total_increase(@RequestBody List<Integer> user, int increase_point){
+    public List<User> total_increase(@RequestBody AdminDto adminDto){
 
-        System.out.println(user.size());
+        // 유저의 holding point 늘려주기
+
+        List<Long> user = adminDto.getUser();
+        int increase_point = adminDto.getIncrease_point();
+
+        for(int i=0; i<user.size(); i++){
+            User Temp_user = userRepository.findByuserId(user.get(i));
+            Temp_user.getPoint().setHoldingPoint(Temp_user.getPoint().getHoldingPoint()+increase_point);
+        }
 
         return userRepository.findAll();
 
@@ -49,11 +57,20 @@ public class AdminController {
 
 
     // 일괄 초기화 -> 0
-
+    @Transactional
     @PostMapping("/admin/total_init")
     @Modifying
-    @Query(value = "UPDATE User u SET u.point.holdingPoint = 0")
     public List<User> total_init(){
+
+        // 교실 고유 키 필요함
+
+        ClassRoom classRoom = classRoomRepository.findByclassRoomId(1L);
+        List<User> users = userRepository.findByClassRoom(classRoom);
+
+        for(int i=0; i<users.size(); i++){
+            users.get(i).getPoint().setHoldingPoint(0);
+        }
+
         return userRepository.findAll();
 
     }
