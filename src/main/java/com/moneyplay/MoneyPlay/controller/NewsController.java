826ca4fa81030058241b07c053d3a1057f58e160
YEJIN325 <seoyj0325@naver.com>
2023-08-16 11:41:51 +0900

    Fix: axios 연결 설정

--- src/main/java/com/moneyplay/MoneyPlay/controller/NewsController.java
+++ src/main/java/com/moneyplay/MoneyPlay/controller/NewsController.java
@@ -4,13 +4,11 @@ import com.moneyplay.MoneyPlay.domain.News;
 import com.moneyplay.MoneyPlay.service.NewsService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/news")
 public class NewsController {
