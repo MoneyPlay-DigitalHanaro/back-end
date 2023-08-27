c1941475d8dcabbeaa84324b60f96ecb66e46174
IkHwan Kim <52392858+dlrghks2090@users.noreply.github.com>
2023-08-27 20:54:48 +0900

    Stock (#58)
    * Develop (#39)
    
    * #8 fix : 추가정보 입력 페이지 완료  (#33)
    
    * #8 feat : kakao 로그인 디비 확인 저장 jwt 생성
    
    * #6 fix 로그인 순서 바꿈
    
    * feat : 추가정보입력 페이지와 연동
    
    * Add : 게시판 조회 기능 완료 (#34)
    
    * Add: 관리자 포인트 기능 완료
    
    * Add : 단어 기능 및 스케쥴러 추가 완료
    
    * Add : 게시판 기능 완료 (#36)
    
    * Add: 관리자 포인트 기능 완료
    
    * Add : 단어 기능 및 스케쥴러 추가 완료
    
    * Add : 게시판 기능 완료
    
    * feat : 관리자에게 일별 포인트 데이터 전달 완료 (#37)
    
    
    * Add: 관리자 포인트 기능 완료
    
    * Add : 단어 기능 및 스케쥴러 추가 완료
    
    * Add : 게시판 기능 완료
    
    ---------
    
    Co-authored-by: Cfctor9 <96967422+Cfctor9@users.noreply.github.com>
    Co-authored-by: Bini <76957075+YOOBINNOH@users.noreply.github.com>
    
    * refactor : 주식 컨트롤러 메서드 이름 수정
    
    * feat : 주식 매수 로직 구현
    
    * #41 #42 feat : 주식 리스트 페이지 구현, 주식 매수 기능 구현
    
    * refactor : 주식 리스트 페이지의 내 주식 수익률 부분 계산 수정
    
    * #13 #52 feat : 주식 매도 기능 완성
    
    * feat : 마이 페이지 주식 정보까지 정리
    
    * refactor : repository 코드 오류 해결
    
    ---------
    
    Co-authored-by: Cfctor9 <96967422+Cfctor9@users.noreply.github.com>
    Co-authored-by: Bini <76957075+YOOBINNOH@users.noreply.github.com>

--- src/main/java/com/moneyplay/MoneyPlay/repository/DepositRepository/DepositRepository.java
+++ src/main/java/com/moneyplay/MoneyPlay/repository/DepositRepository/DepositRepository.java
@@ -1,11 +1,12 @@
 package com.moneyplay.MoneyPlay.repository.DepositRepository;
 
 import com.moneyplay.MoneyPlay.domain.Deposit;
+import com.moneyplay.MoneyPlay.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
 public interface DepositRepository extends JpaRepository<Deposit,Long> {
 
-    Optional<Deposit> findByUserId(Long userId);
+    Optional<Deposit> findByUser(User user);
 }
