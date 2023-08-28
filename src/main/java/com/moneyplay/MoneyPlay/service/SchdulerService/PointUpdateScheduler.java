e72bd89b7652d2848eac6bff96952c3d01123dc9
dlrghks2090 <dlrghks2090@gmail.com>
2023-08-28 15:50:12 +0900

    feat : 스케줄러 작성

--- src/main/java/com/moneyplay/MoneyPlay/service/SchdulerService/PointUpdateScheduler.java
+++ src/main/java/com/moneyplay/MoneyPlay/service/SchdulerService/PointUpdateScheduler.java
@@ -1,4 +1,4 @@
-package com.moneyplay.MoneyPlay.service.DailyService;
+package com.moneyplay.MoneyPlay.service.SchdulerService;
 
 import com.moneyplay.MoneyPlay.domain.ClassDailyPoint;
 import com.moneyplay.MoneyPlay.domain.ClassRoom;
