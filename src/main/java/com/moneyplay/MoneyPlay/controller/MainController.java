1986fd7e53a777c505bf8c4f8a5678102c5ce51d
dlrghks2090 <dlrghks2090@gmail.com>
2023-08-29 10:07:54 +0900

    feat : 메인 화면 기능 구현중

---
+++ src/main/java/com/moneyplay/MoneyPlay/controller/MainController.java
@@ -0,0 +1,34 @@
+package com.moneyplay.MoneyPlay.controller;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.interfaces.DecodedJWT;
+import com.moneyplay.MoneyPlay.domain.User;
+import com.moneyplay.MoneyPlay.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/main")
+public class MainController {
+
+    private final UserRepository userRepository;
+
+    public ResponseEntity<?> mainData(@RequestHeader("Authorization") String tokens) {
+        // 헤더에서 토큰을 가져온다.
+        String token =tokens.substring(7);
+        // 토큰값을 decode하여 id값을 가져온다.
+        DecodedJWT decodedJWT = JWT.decode(token);
+        Long userId = decodedJWT.getClaim("id").asLong();
+
+        User user = userRepository.findByuserId(userId);
+
+        List<User> userList = userRepository.findAll();
+
+    }
+}
